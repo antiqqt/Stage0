5f3c6ec2f738c62e4fcdf097e4d7c9a108032801
Anton <antonpomogaev@gmail.com>
2022-02-14 19:01:32 +0300

    feat: add translation and image change

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,29 +1,96 @@
 // Get data from quotes API
-const url = 'https://api.icndb.com/jokes/random';
-const quote = document.querySelector('.quote');
+const url = 'https://api.icndb.com/jokes';
+const quoteEn = document.querySelector('.quote-en');
+const quoteRu = document.querySelector('.quote-ru');
 
 async function getData() {
   const res = await fetch(url);
   const data = await res.json();
+  let currentJokeIndex;
+  let currentRuJokeIndex;
   // Change the joke
   showData(data);
   // Change text to be shared
   changeSharedText(data);
 }
-getData();
 
 function showData(data) {
+  let jokeIndex = randomIndex(0, data.value.length);
+  currentJokeIndex = jokeIndex;
   // Remove &quote symbols
-  const joke = data.value.joke.replace(/&quot;/g, '"');
-  quote.textContent = joke;
+  const joke = data.value[jokeIndex].joke.replace(/&quot;/g, '"');
+  quoteEn.textContent = joke;
 }
 
 function changeSharedText(data) {
   const socialShare = document.querySelector('.yandex-share');
-  socialShare.setAttribute('data-title', data.value.joke);
+  socialShare.setAttribute(
+    'data-description',
+    data.value[currentJokeIndex].joke
+  );
 }
 
-// Make text change when button is pressed
+// Make text and image change on button press
 const button = document.querySelector('.button');
 
-button.addEventListener('click', getData);
+let imgCurrentIndex = 1; // save original picture index
+
+function changeImg() {
+  const mainImg = document.querySelector('.img');
+  mainImg.src = `assets/img/chuck-norris-${getNewIndex()}-removebg.png`;
+}
+
+function getNewIndex() {
+  let newIndex;
+
+  do {
+    newIndex = randomIndex(1, 5);
+  } while (newIndex === imgCurrentIndex);
+
+  imgCurrentIndex = newIndex;
+
+  return newIndex;
+}
+
+function randomIndex(min, max) {
+  // random num between both min and max included
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+button.addEventListener('click', changeImg);
+
+// Change language
+const switchLang = document.querySelector('.switch-input');
+
+async function getTranslatedQuotes() {
+  const quotes = 'assets/jokes/chuck-jokes-ru.json';
+  const res = await fetch(quotes);
+  const data = await res.json();
+  showRuData(data);
+  changeSharedText(data);
+}
+
+function showRuData(data) {
+  let jokeIndex = randomIndex(0, data.value.length);
+  currentRuJokeIndex = jokeIndex;
+  // Remove &quote symbols
+  const joke = data.value[jokeIndex].joke.replace(/&quot;/g, '"');
+  quoteRu.textContent = joke;
+}
+
+
+
+// Starting script
+getData();
+getTranslatedQuotes();
+
+button.addEventListener('click', () => {
+  getData();
+  getTranslatedQuotes();
+});
+
+switchLang.addEventListener('change', () => {
+  document
+    .querySelectorAll('.quote')
+    .forEach((e) => e.classList.toggle('visually-hidden'));
+});
